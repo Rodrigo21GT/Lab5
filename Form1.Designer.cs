4084d8030386bf7a73b10cc8c712487673ae9789
Rodrigo Escobar <rodrigoescno2002@gmail.com>
2024-03-11 08:58:37 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Repaso__1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(403, 345);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(438, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(407, 345);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Leer o cargar datos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(861, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 62;
+            this.dataGridView3.RowTemplate.Height = 28;
+            this.dataGridView3.Size = new System.Drawing.Size(356, 345);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(346, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 54);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reporte";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(532, 385);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(268, 28);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(532, 433);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 40);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Mostrar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(896, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 573);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso__1
+{
+    public partial class Form1 : Form
+    {
+        List<Empleado> empleados = new List<Empleado>();
+        List<Asistencia> asistencias = new List<Asistencia>();
+        List<Reporte> reportes = new List<Reporte>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MostrarEmpleados();
+            CargarEmpleados();
+            MostrarAsistencia();
+
+        }
+        private void MostrarEmpleados()
+        {
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = empleados;
+            dataGridView1.Refresh();
+        }
+        private void CargarEmpleados()
+        {
+            string fileName = "Datos.txt";
+
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+
+            {
+                Empleado empleado = new Empleado();
+                empleado.NoEmpleado = Convert.ToInt32(reader.ReadLine());
+                empleado.Nombre = reader.ReadLine();
+                empleado.Sueldo = Convert.ToDecimal(reader.ReadLine());
+
+                //Guardar el empleado a la lista de empleados
+                empleados.Add(empleado);
+            }
+            reader.Close();
+        }
+        public void CargarAsistencia()
+        {
+            string fileName = "Horas.txt";
+
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+
+            {
+                Asistencia asistencia = new Asistencia();
+                asistencia.NoEmpleado = Convert.ToInt32(reader.ReadLine());
+                asistencia.HorasMes = Convert.ToInt32(reader.ReadLine());
+                asistencia.Mes = Convert.ToInt32(reader.ReadLine());
+
+                //Guardar el empleado a la lista de empleados
+                asistencias.Add(asistencia);
+            }
+        }
+        private void MostrarAsistencia()
+        {
+            dataGridView2.DataSource = null;
+            dataGridView2.DataSource = asistencias;
+            dataGridView2.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach(Empleado empleado in empleados)
+            {
+                int noEmpleado = empleado.NoEmpleado;
+
+                foreach(Asistencia asistencia in asistencias)
+                {
+                    if(empleado.NoEmpleado == asistencia.NoEmpleado)
+                    {
+                        Reporte reporte = new Reporte();
+                        reporte.NombreEmpleado = empleado.Nombre;
+                        reporte.Mes = asistencia.Mes;
+                        reporte.SueldoMensual = empleado.Sueldo * asistencia.HorasMes;
+
+                        reportes.Add(reporte);
+                    }
+                }
+            }
+            dataGridView3.DataSource=null;
+            dataGridView3.DataSource = reportes;
+            dataGridView3.Refresh();
+        }
+        private void CargarReporte()
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarEmpleados();
+            CargarAsistencia();
+            comboBox1.DisplayMember = "Nombre";
+            comboBox1.ValueMember = "NoEmpleados";
+            comboBox1.DataSource = empleados;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int noEmpleados = Convert.ToInt32(comboBox1.SelectedValue); 
+
+
+            Empleado empleadoEncontrado = empleados.Find(c => c.NoEmpleado == noEmpleados);
+            Asistencia asistenciaEncontrada = asistencias.Find(u => u.NoEmpleado == noEmpleados);
+
+            decimal sueldo = empleadoEncontrado.Sueldo = asistenciaEncontrada.HorasMes;
+            label1.Text = empleadoEncontrado.Nombre;
+            label1.Text = sueldo.ToString();
+        }
+    }
+}
